24da4343c999d40c672198dbf8f39c9c42824e4a
RendyCristianBoen <Randboy205@gmail.com>
2026-02-03 20:04:53 +0700

    week01: initial setup and procedural code

---
+++ src/oop_130679_RendyCristianBoen/week01/GradingSystem.kt
@@ -0,0 +1,8 @@
+package oop_130679_RendyCristianBoen.week01
+
+fun main() {
+    var name: String = "Rendy Crsitian"
+    var score: Int = 80
+
+    println("Nama: " + name + ", Nilai: " + score)
+}
