1ddaca4a5b75dfe2a943bf5f0f0b75772501f3f4
RendyCristianBoen <Randboy205@gmail.com>
2026-02-20 22:28:19 +0700

    week03: (task 2) implement Player testing in main

--- src/oop_130679_RendyCristianBoen/week03/MainWeapon.kt
+++ src/oop_130679_RendyCristianBoen/week03/MainWeapon.kt
@@ -2,6 +2,10 @@ package oop_130679_RendyCristianBoen.week03
 
 fun main() {
 
+    val player = Player("Rendy")
+    player.addXp(50)
+    player.addXp(60)
+
     val weapon = Weapon("Dragon Slayer")
 
     weapon.damage = -50
