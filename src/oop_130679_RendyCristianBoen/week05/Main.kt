b8e0cee2a3d30bd2e99458a9993392ced85a6b69
RendyCristianBoen <Randboy205@gmail.com>
2026-03-04 19:55:43 +0700

    week05: implement polymorphic collection and smart casting

---
+++ src/oop_130679_RendyCristianBoen/week05/Main.kt
@@ -0,0 +1,21 @@
+package oop_130679_RendyCristianBoen.week05
+
+fun main() {
+
+    val daftarPegawai: List<Pegawai> = listOf(
+        Dosen("Pak Budi"),
+        Admin("Bu Sinta")
+    )
+
+    for (pegawai in daftarPegawai) {
+
+        pegawai.bekerja()
+
+        when (pegawai) {
+            is Dosen -> pegawai.mengajar()
+            is Admin -> pegawai.doAdminWork()
+        }
+
+        println("---------------")
+    }
+}
