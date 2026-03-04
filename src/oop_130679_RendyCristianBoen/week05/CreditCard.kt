5fb6f1f7b00d62a41d7503629d6e64043e476c5e
RendyCristianBoen <Randboy205@gmail.com>
2026-03-04 20:26:34 +0700

    week05: (task 2) create CreditCard class

---
+++ src/oop_130679_RendyCristianBoen/week05/CreditCard.kt
@@ -0,0 +1,16 @@
+package oop_130679_RendyCristianBoen.week05
+
+class CreditCard(accountName: String, val limit: Double)
+    : PaymentMethod(accountName) {
+
+    var usedAmount: Double = 0.0
+
+    override fun processPayment(amount: Double) {
+        if (usedAmount + amount <= limit) {
+            usedAmount += amount
+            println("Transaksi berhasil. Total terpakai: $usedAmount")
+        } else {
+            println("Transaksi ditolak. Melebihi limit.")
+        }
+    }
+}
