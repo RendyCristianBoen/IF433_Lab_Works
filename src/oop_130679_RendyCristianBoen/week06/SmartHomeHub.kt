7cba816a53123a49f5cec1e23c11c1e5f8c5f2d9
RendyCristianBoen <Randboy205@gmail.com>
2026-03-10 23:50:02 +0700

    week06: (task) create SmartHomeHub logic

--- src/oop_130679_RendyCristianBoen/week06/SmartHomeHub.kt
+++ src/oop_130679_RendyCristianBoen/week06/SmartHomeHub.kt
@@ -14,15 +14,4 @@ class SmartHomeHub {
             }
         }
     }
-
-    fun activateSecurityMode() {
-        for (device in devices) {
-            if (device is Recordable) {
-                device.startRecord()
-            }
-            if (device is SmartSpeaker) {
-                device.playMusic("Sirine Peringatan")
-            }
-        }
-    }
 }
