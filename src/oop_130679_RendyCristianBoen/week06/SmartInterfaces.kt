635170b7db5692bea83de0c9b3cbf469da2e46c9
RendyCristianBoen <Randboy205@gmail.com>
2026-03-10 23:48:02 +0700

    week06: (task) create smart home interfaces

---
+++ src/oop_130679_RendyCristianBoen/week06/SmartInterfaces.kt
@@ -0,0 +1,18 @@
+package oop_130679_RendyCristianBoen.week06
+
+interface SmartDevice {
+    val id: String
+    val name: String
+}
+
+interface Switchable {
+    fun turnOn()
+    fun turnOff()
+}
+
+interface Recordable {
+    fun startRecord()
+    fun stopRecord() {
+        println("Perekaman dihentikan dan disimpan ke Cloud.")
+    }
+}
