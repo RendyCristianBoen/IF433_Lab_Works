be1dd4e76d10b5b006c785fed1f0dd33826f0ee5
RendyCristianBoen <Randboy205@gmail.com>
2026-03-10 23:47:46 +0700

    week06: implement decoupled processCheckout in main

---
+++ src/oop_130679_RendyCristianBoen/week06/Main.kt
@@ -0,0 +1,36 @@
+package oop_130679_RendyCristianBoen.week06
+
+fun processCheckout(method: PaymentMethod, amount: Double) {
+    println("-> Memulai checkout...")
+    method.pay(amount)
+}
+
+fun main() {
+    val myWatch = Smartwatch()
+    myWatch.showTime()
+
+    val myPhone = Smartphone()
+    myPhone.turnOn()
+
+    val pay1 = Gopay()
+    val pay2 = CreditCard()
+
+    println("\n=== TESTING CHECKOUT ===")
+    processCheckout(method = pay1, amount = 50000.0)
+    processCheckout(method = pay2, amount = 150000.0)
+
+    val lamp = SmartLamp("001", "Ruang Tamu")
+    val speaker = SmartSpeaker("002", "Google Nest Dapur")
+    val cctv = SmartCCTV("003", "Ezviz Garasi")
+
+    val hub = SmartHomeHub()
+    hub.addDevice(lamp)
+    hub.addDevice(speaker)
+    hub.addDevice(cctv)
+
+    println("\n=== ACTIVATE SECURITY MODE ===")
+    hub.activateSecurityMode()
+
+    println("\n=== TURN OFF ALL SWITCHES ===")
+    hub.turnOffAllSwitches()
+}
