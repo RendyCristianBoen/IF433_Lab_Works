7b85526fc8ccd3fa9964f2d6b008f235794b5257
RendyCristianBoen <Randboy205@gmail.com>
2026-04-14 22:24:39 +0700

    week08: (task) create JavaPaymentService for payment interop

---
+++ src/oop_130679_RendyCristianBoen/week08/JavaPaymentService.java
@@ -0,0 +1,7 @@
+package oop_130679_RendyCristianBoen.week08;
+
+public class JavaPaymentService {
+    public static String processPayment(String productId) {
+        return "TRX-" + productId + "-SUCCESS";
+    }
+}
