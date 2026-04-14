bc84b46252eab043bc89af71d58be12b0efc5b13
RendyCristianBoen <Randboy205@gmail.com>
2026-04-14 15:43:45 +0700

    week08: implement safe call with let block

--- src/oop_130679_RendyCristianBoen/week08/Main.kt
+++ src/oop_130679_RendyCristianBoen/week08/Main.kt
@@ -3,7 +3,11 @@ package oop_130679_RendyCristianBoen.week08
 fun main() {
 
     val order = Order(Customer(Address("Bandung")))
-
     val city = order.customer?.address?.city ?: "Unknown"
     println("City: $city")
+
+    order.customer?.address?.city?.let {
+        println("City with let: $it")
+    }
+
 }
