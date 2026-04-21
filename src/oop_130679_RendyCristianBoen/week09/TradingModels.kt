f8d5abea0c4ed3d47c3470e10717a1623cfcd536
RendyCristianBoen <Randboy205@gmail.com>
2026-04-21 23:08:13 +0700

    week09: (task) create TradeLog data class

---
+++ src/oop_130679_RendyCristianBoen/week09/TradingModels.kt
@@ -0,0 +1,10 @@
+package oop_130679_RendyCristianBoen.week09
+
+data class TradeLog(
+    val pair: String,
+    val position: String,
+    val leverage: Int,
+    val roe: Double,
+    val status: String
+)
+
