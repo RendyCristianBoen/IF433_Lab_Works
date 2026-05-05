421ba8dea41fcdd67a399cd7f59b47a617a97028
RendyCristianBoen <Randboy205@gmail.com>
2026-05-05 15:17:49 +0700

    week11: (task) configure Ezviz camera with also

--- src/oop_130679_RendyCristianBoen/week11/Smarthomedashboard.kt
+++ src/oop_130679_RendyCristianBoen/week11/Smarthomedashboard.kt
@@ -10,3 +10,15 @@ fun main() {
     }.also {
         homeDevices.add(it)
     }
+
+    SmartDevice("Ezviz Outdoor", "Camera").apply {
+        isOnline = true
+        powerLoad = 5
+    }.also {
+        println("(LOG) Kamera terhubung")
+        homeDevices.add(it)
+    }
+
+
+
+}
